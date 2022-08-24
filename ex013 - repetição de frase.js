62dd6f4f5d25ffc61763bd59ba207a17d77e92e0
Tati <104369092+Tatisd@users.noreply.github.com>
2022-08-24 12:57:30 -0300

    Add files via upload

---
+++ ex013 - repetição de frase.js
@@ -0,0 +1,20 @@
+var c = 1
+while(c <6) {
+    console.log('Tudo bem?')
+    c++ //c = c + 1
+}
+
+// o comando vai repetir a frase "tudo bem?" 5 vezes
+
+var c = 1
+while(c <= 3) {
+    console.log(`Passo ${c}`)
+    c++ 
+} // esse comando escreve passo 1, passo 2, passo 3
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}
+while (c <= 6) //esse comando escreve os passos como no comando anterior só que usando do/ while
